39acd9db1259879a86b96aaa32c63b93d1538728
24LP4462 24LP4462 <24LP4462@G301-CC0194.local>
2025-04-07 19:17:00 +0800

    dd

---
+++ by_me_coffee_frontend/src/app/(profile)/profile/_features/BankCard.tsx
@@ -0,0 +1,18 @@
+import { UseFormReturn } from "react-hook-form";
+
+export const BankCard = ({form}:{form:UseFormReturn<{
+    avatarImage: string;
+    name: string;
+    about: string;
+    socialMediaURL: string;
+}, {
+    avatarImage: string;
+    name: string;
+    about: string;
+    socialMediaURL: string;
+}>}) => {
+    return(
+        <div></div>
+    )
+}
+export default BankCard
