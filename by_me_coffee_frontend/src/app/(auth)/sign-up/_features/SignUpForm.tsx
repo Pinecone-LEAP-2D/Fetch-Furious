e34062736151f07d9fec7bb7667a2e66b3dfff3c
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-04-07 18:58:03 +0800

    sign up server api

--- by_me_coffee_frontend/src/app/(auth)/sign-up/_features/SignUpForm.tsx
+++ by_me_coffee_frontend/src/app/(auth)/sign-up/_features/SignUpForm.tsx
@@ -8,11 +8,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import UserName from "../_components/UserName";
 import Email from "../_components/EmailForm";
-const signUpSchema = z.object({
-  email: z.string().email("Invalid email address"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-  username: z.string().min(3, "Username must be at least 3 characters"),
-});
+import { signUpSchema } from "@/app/api/auth/sign-up/route";
 export default function SignUpForm() {
   const router = useRouter();
   const [step, setStep] = useState<"username" | "emailAndPassword">("username");
