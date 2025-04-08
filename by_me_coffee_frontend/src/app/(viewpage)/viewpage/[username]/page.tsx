f3bc662324b49e712e359f72aa5d7d691d83707c
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-04-08 20:13:18 +0800

    userprovider

--- by_me_coffee_frontend/src/app/(viewpage)/viewpage/[username]/page.tsx
+++ by_me_coffee_frontend/src/app/(viewpage)/viewpage/[username]/page.tsx
@@ -9,19 +9,22 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const { username } = useParams();
-  const [profile, setProfile] = useState<Profile>()
+  const [profile, setProfile] = useState<Profile>();
   const fetchProfile = async () => {
     try {
-        const response = await getProfile(8)
-        setProfile(response?.data.result)
+      const response = await getProfile(8);
+      console.log(response);
+      setProfile(response?.data.result);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-  }
+  };
   useEffect(() => {
-    fetchProfile()
-  },[]);
-
+    fetchProfile();
+  }, []);
+  if (!profile?.avatarImage) {
+    return <div>user not found</div>;
+  }
   return (
     <div className="w-scree items-center flex flex-col">
       <div className="w-full h-[500px]">
@@ -37,21 +40,32 @@ export default function Home() {
         <div className="flex flex-col gap-5 w-full">
           <div className="p-6 border rounded-lg flex flex-col gap-6">
             <div className="w-full flex justify-between border-b border-[#E4E4E7] pb-8">
-                <div className="flex gap-3 items-center">
-                    <Image alt="avatarImage" height={12} width={12} src={'/vercel.svg'}  className="border w-12 h-12 rounded-full"/>
-                    <div className="text-xl font-semibold">{username}</div>
-                </div>
-                <Button variant='secondary' className="cursor-pointer">Edit Page</Button>
+              <div className="flex gap-3 items-center">
+                <Image
+                  alt="avatarImage"
+                  height={12}
+                  width={12}
+                  src={profile.avatarImage}
+                  className="border w-12 h-12 rounded-full"
+                />
+                <div className="text-xl font-semibold">{username}</div>
+              </div>
+              <Button variant="secondary" className="cursor-pointer">
+                Edit Page
+              </Button>
             </div>
-            <div>
-                <p className="font-semibold text-lg">About {username}</p>
-                <div>{profile?.about}</div>
+            <div className="flex flex-col gap-6">
+              <p className="font-semibold text-lg">About {username}</p>
+              <div>{profile?.about}</div>
             </div>
+          </div>
+          <div className="p-6 border rounded-lg flex flex-col gap-6">
+            <div className="text-lg font-semibold">Social Media URL</div>
+            <div>{profile?.socialMediaURL}</div>
           </div>
           <div className="p-6 border rounded-lg">
-            <div className="text-lg font-semibold">Social Media URL</div>
+          <div className="text-lg font-semibold">Recent supporters</div>
           </div>
-          <div className="p-6 border rounded-lg"></div>
         </div>
         <div className="flex flex-col gap-5 w-full border rounded-lg">
           <div className="p-6"></div>
--- by_me_coffee_frontend/src/utils/request.tsx
+++ by_me_coffee_frontend/src/utils/request.tsx
@@ -1,14 +1,15 @@
+
 import { profileSchema } from "@/app/(profile)/profile/_features/Profile";
 import axios from "axios";
 import { z } from "zod";
 
-export const postProfile = async (values: z.infer<typeof profileSchema>) => {
+export const postProfile = async (values: z.infer<typeof profileSchema>, image:string) => {
   const token = localStorage.getItem("token");
   try {
     const response = await axios.post(
       "http://localhost:4000/profile",
       {
-        avatarImage: values.avatarImage,
+        avatarImage: image,
         name: values.name,
         about: values.about,
         socialMediaURL: values.socialMediaURL,
