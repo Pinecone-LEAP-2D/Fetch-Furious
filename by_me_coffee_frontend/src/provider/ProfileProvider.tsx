6d3be98a058536b08f0f4bc06d4cef4713ac6156
SunshineAlis <bumaam@yahoo.com>
2025-04-09 18:01:49 +0800

    1

--- by_me_coffee_frontend/src/provider/ProfileProvider.tsx
+++ by_me_coffee_frontend/src/provider/ProfileProvider.tsx
@@ -10,20 +10,21 @@ import {
 import { jwtDecode } from "jwt-decode";
 import { JwtPayload } from "jsonwebtoken";
 import { addBackground, getProfile } from "@/utils/request";
-import { Profile } from "@prisma/client";
+
 import { useRouter } from "next/navigation";
+import { Profile } from "@prisma/client";
 
 type ProfileContext = {
   profile: Profile | undefined;
-  addBackgroundImage : (image:string)=>void
+  addBackgroundImage: (image: string) => void;
 };
 const ProfileContex = createContext<ProfileContext | null>(null);
 
 export const ProfileProvider = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState(false);
-  const router = useRouter()
+  const router = useRouter();
   const [profile, setProfile] = useState<Profile>();
-  const [userID, setUserID] = useState('')
+  const [userID, setUserID] = useState("");
   const fetchProfile = async () => {
     setLoading(true);
     const token = localStorage.getItem("token");
@@ -32,32 +33,31 @@ export const ProfileProvider = ({ children }: { children: ReactNode }) => {
     try {
       const response = await getProfile(decode.userId);
       if (!response?.data.result) {
-        router.push('/profile')
+        router.push("/profile");
         return;
-      } 
+      }
       await setProfile(response.data.result);
-      setUserID(decode.userId)
+      setUserID(decode.userId);
     } catch (error) {
       console.log(error);
-    
     } finally {
       setLoading(false);
     }
   };
   useEffect(() => {
     fetchProfile();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-const addBackgroundImage = async (image:string,) => {
+  const addBackgroundImage = async (image: string) => {
     try {
-        setLoading(true)
-        await addBackground(image, userID)
+      setLoading(true);
+      await addBackground(image, userID);
     } catch (error) {
-        console.log(error);
-    }finally {
-        setLoading(false)
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-}
+  };
   return (
     <ProfileContex.Provider value={{ profile, addBackgroundImage }}>
       {children}
